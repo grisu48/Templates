b603a04449cac43096be47330890e9c39705906d
phoenix <felix.niederwanger@suse.com>
2020-03-30 11:22:47 +0200

    openQA test template

---
+++ Templates/SUSE/openQA.pm
@@ -0,0 +1,30 @@
+# SUSE's openQA tests
+#
+# Copyright © 2020 SUSE LLC
+#
+# Copying and distribution of this file, with or without modification,
+# are permitted in any medium without royalty provided the copyright
+# notice and this notice are preserved.  This file is offered as-is,
+# without any warranty.
+
+# Summary: [...]
+# Maintainer: Felix Niederwanger <felix.niederwanger@suse.de>
+
+use base 'consoletest';
+use strict;
+use warnings;
+use testapi;
+use utils;
+use version_utils;
+
+sub run {
+    # Preparation
+    my $self = shift;
+    $self->select_serial_terminal;
+    # install requirements
+    zypper_call 'in [PACKAGEs]';
+    assert_script_run 'echo Hello World';
+    validate_script_output "echo Hello World", sub { m/Hello World/ };
+}
+
+1;
