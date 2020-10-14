864ada70871087ec9cf0034280b8087e29b7227d
felix.niederwanger@suse.com <phoenix>
2020-10-14 14:03:41 +0200

    Add go test template file

---
+++ Templates/Go/test.go
@@ -0,0 +1,11 @@
+/*
+ *
+ */
+package main
+
+import "testing"
+
+// Name must be TestXXX (replace XXX with whatever)
+func TestMyAwesomeFunction(t *testing.T) {
+    // Write your test stuff here
+}
