50dd7d24e5ec4767b9b844bf4eab46e0a4f7d405
felix.niederwanger@suse.com <phoenix>
2021-01-07 08:38:31 +0100

    Update openQA template

--- Templates/SUSE/openQA/console-test.pm
+++ Templates/SUSE/openQA/console-test.pm
@@ -15,26 +15,31 @@ use strict;
 use warnings;
 use testapi;
 use utils;
-use version_utils;
+#use version_utils qw(is_sle is_opensuse is_leap is_tumbleweed);
+#use registration qw(cleanup_registration register_product add_suseconnect_product get_addon_fullname remove_suseconnect_product);
 
 sub run {
     # Preparation
-    my $self = shift;
+    my $self = shift; # $_[0];
     #select_console 'root-console';
     $self->select_serial_terminal;
-    # install requirements
     zypper_call 'in [PACKAGEs]';
+    # Run some tests
     assert_script_run 'echo Hello World';
     validate_script_output "echo Hello World", sub { m/Hello World/ };
 }
 
 
 sub post_fail_hook {
-
+    my $self = shift;
+	# ... 
+	$self->SUPER::post_fail_hook;
 }
 
 sub post_run_hook {
-
+    my $self = shift;
+    # ...
+    $self->SUPER::post_run_hook;
 }
 
 1;
